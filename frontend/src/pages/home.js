efcb9d84fa48d316fe4fcdefe9dc6f50e33129f8
Shana <maryjop10@gmail.com>
2022-05-11 11:07:24 -0500

    Header responsive

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import '../styles/home.css';
+import game from '../images/gameover.png';
+import {RiArrowDropDownLine} from 'react-icons/ri';
 
 const Home = () => {
     return (
         <>
-            <h1>Home page</h1>
-            <h1>Home page</h1>
-            <h1>Home page</h1>
-            <h1>Home page</h1>
+            <div className="header section__padding">
+                <div className="header_content">
+                    <h1>Welcome Gamer!</h1>
+                    <h1>Welcome Gamer!</h1>
+                    <h1>Welcome Gamer!</h1>
+                    <h2>Do you want to be part of the Gamming World?</h2>
+                    <p>Come and join this wonderful community.</p>
+                </div>
+                <div className="header_image">
+                    <img src={game} />
+                </div>
+            </div>
         </>
-    )
+    );
 }
 export default Home; 
