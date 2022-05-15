bf3fed4de14ab69c6153fcd88874c1119143abfa
Shana <maryjop10@gmail.com>
2022-05-15 01:15:00 -0500

    Home Banner

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,6 +2,10 @@ import React from 'react';
 import '../styles/home.css';
 import game from '../images/gameover.png';
 import wpp from '../images/wpp.png';
+import img1 from '../images/img1.jpg';
+import img2 from '../images/img2.jpg';
+import img3 from '../images/img3.jpg';
+import { Slideshow, Slide } from '../components/Slide/slide';
 
 const Home = () => {
     return (
@@ -19,10 +23,21 @@ const Home = () => {
                 </div>
             </div>
             <div>
+                <Slideshow controles={true}>
+                    <Slide>
+                            <img src={img1} alt="" />
+                    </Slide>
+                    <Slide>
+                            <img src={img2} alt="" />
+                    </Slide>
+                    <Slide>
+                            <img src={img3} alt="" />
+                    </Slide>
+                </Slideshow>
             </div>
             <div className="whatsapp" >
                 <a href="https://api.whatsapp.com/send?phone=573053521012" target="_blank">
-                    <img src={wpp} alt=""/>
+                    <img src={wpp} alt="" />
                 </a>
             </div>
         </>
