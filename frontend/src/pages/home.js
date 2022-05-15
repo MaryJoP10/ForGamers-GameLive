71d12b6d277819ff1467509c9a57fb46d235515b
Shana <maryjop10@gmail.com>
2022-05-15 16:24:58 -0500

    Banner fixed 2

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,6 +3,10 @@ import '../styles/home.css';
 import game from '../images/gameover.png';
 import wpp from '../images/wpp.png';
 import Slider from "../components/Slide/slide";
+import img1 from "../images/img1.jpg";
+import img2 from "../images/img2.jpg";
+import img3 from "../images/img3.jpg";
+
 
 const Home = () => {
     return (
@@ -20,7 +24,27 @@ const Home = () => {
                 </div>
             </div >
             <div>
-                <Slider/>
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={img1} class="d-block w-100" alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={img2} class="d-block w-100" alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={img3} class="d-block w-100" alt="..."/>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
             <div className="whatsapp" >
                 <a href="https://api.whatsapp.com/send?phone=573053521012" target="_blank">
