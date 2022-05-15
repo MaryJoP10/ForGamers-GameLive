74a009801ce786829ac8de2df0a4525f891e71c6
Shana <maryjop10@gmail.com>
2022-05-15 00:55:40 -0500

    Login and Register with localstorage

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import wpp from '../images/wpp.png';
 import crash from '../images/crash.png';
 import '../styles/signin.css';
 
-const Signin = () => {
+function Signin() {
+    const [usernamelog, setUsernamelog] = useState("");
+    const [passwordlog, setPasswordlog] = useState("");
+
+    const [flag, setFlag] = useState(false);
+
+    function handleLogin(e) {
+        e.preventDefault();
+        let pass = localStorage
+            .getItem("password")
+            .replace(/"/g, "");
+        let user = localStorage.getItem("username").replace(/"/g, "");
+
+
+        if (!usernamelog || !passwordlog) {
+            setFlag(true);
+            alert("Enter your username and password to login");
+        } else if (passwordlog !== pass || usernamelog !== user) {
+            setFlag(true);
+            alert("Wrong password or username")
+        } else {
+            setFlag(false);
+            alert("Successful Login")
+            setUsernamelog("");
+            setPasswordlog("");
+        }
+    }
     return (
         <>
             <div className='signin_page'>
-                <img src={crash}/>
-                <div className="signin_container">
-                    <form>
-                        <h1>Sign In</h1>
-                        <div className='input_signin'>
-                            <div className='label'>
-                                <input type='text'required/>
-                                <label> Username</label>
+                <img src={crash} />
+                <div className="signin_container" >
+                        <form onSubmit={handleLogin}>
+                            <h1>Sign In</h1>
+                            <div className='input_signin'>
+                                <div className='label'>
+                                    <input type='text' value={usernamelog} onChange={(event) => setUsernamelog(event.target.value)} required />
+                                    <label> Username</label>
+                                </div>
+                                <div className='label'>
+                                    <input type='password' value={passwordlog} onChange={(event) => setPasswordlog(event.target.value)} required />
+                                    <label>Password</label>
+                                </div>
+                                <button type='submit'>Sign In</button>
                             </div>
-                            <div className='label'>
-                                <input type='password' required/>
-                                <label>Password</label>
-                                <button type='submit'>Sign Up</button>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
                 </div>
             </div>
 
             <div className="whatsapp" >
                 <a href="https://api.whatsapp.com/send?phone=573053521012" target="_blank">
-                    <img src={wpp} alt=""/>
+                    <img src={wpp} alt="" />
                 </a>
             </div>
         </>
--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import wpp from '../images/wpp.png';
 import '../styles/signup.css';
 import COD from '../images/COD.png';
 
-const Signup = () => {
+function Signup() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [flag, setFlag] = useState(false);
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+
+        if (!username || !email || !password) {
+            setFlag(true);
+        } else {
+            setFlag(false);
+            localStorage.setItem("username", JSON.stringify(username));
+            localStorage.setItem("email", JSON.stringify(email));
+            localStorage.setItem("password", JSON.stringify(password));
+            alert("Successful Sign Up");
+            setUsername("");
+            setEmail("");
+            setPassword("");
+            }
+    }
+
     return (
         <>
             <div className='signup_page'>
-                <img src={COD}/>
+                <img src={COD} />
                 <div className="signup_container">
-                    <form>
-                        <h1>Sign Up</h1>
-                        <div className='input_signup'>
-                            <div className='label'>
-                                <input type='text'required/>
-                                <label> Username</label>
-                            </div>
-                            <div className='label'>
-                                <input type='email' required/>
-                                <label>Email</label>
-                            </div>
-                            <div className='label'>
-                                <input type='password' required/>
-                                <label>Password</label>
+                    {" "}
+                        <form onSubmit={handleFormSubmit}>
+                            <h1>Sign Up</h1>
+                            <div className='input_signup'>
+                                <div className='label'>
+                                    <input type='text' value={username} onChange={(event) => setUsername(event.target.value)} required />
+                                    <label> Username</label>
+                                </div>
+                                <div className='label'>
+                                    <input type='email' value={email} onChange={(event) => setEmail(event.target.value)} required />
+                                    <label>Email</label>
+                                </div>
+                                <div className='label'>
+                                    <input type='password' value={password} onChange={(event) => setPassword(event.target.value)} required />
+                                    <label>Password</label>
+                                </div>
                                 <button type='submit'>Sign Up</button>
+                                
                             </div>
-                        </div>
-                    </form>
+                        </form>
                 </div>
             </div>
-            
+
             <div className="whatsapp" >
                 <a href="https://api.whatsapp.com/send?phone=573053521012" target="_blank">
                     <img src={wpp} alt="" />
